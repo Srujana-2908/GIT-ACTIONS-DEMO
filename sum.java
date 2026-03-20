4d04b1da4a5c38787d21b28eca71eb31d233ecc2
Srujana-2908 <132274615+Srujana-2908@users.noreply.github.com>
2026-03-20 19:44:30 +0530

    Change value of 'a' and output difference
    Updated variable 'a' from 5 to 15 and changed output to display difference.

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 15;
         int b = 10;
-        System.out.println("Sum = " + (a + b));
+        System.out.println("difference = " + (a - b));
     }
 }
