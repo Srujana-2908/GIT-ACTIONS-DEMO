4e041bc5cbed7df8e26fc6cda5094be39542854f
Srujana-2908 <132274615+Srujana-2908@users.noreply.github.com>
2026-03-20 19:56:32 +0530

    Change sum calculation to subtraction

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        System.out.println("Sum = " + (a + b));
+        System.out.println("Sum = " + (a - b));
     }
 }
